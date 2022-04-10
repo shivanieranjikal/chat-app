ba3ce78a1ccc34817d055972591cfca990c31f8a
shivanieranjikal <eranjikal.s@gmail.com>
2022-04-10 23:49:28 +0530

    Change

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { disconnect } = require('process');
 const server=http.createServer(app);
 const {Server}=require("socket.io")
 const io=new Server(server)
+const moment=require('moment')
+
 
 app.get('/',(req,res) => {
     res.sendFile(__dirname + '/index.html');
@@ -15,13 +17,14 @@ console.log('a user connected');
 socket.on('disconnect',() => {
   console.log('user disconnected');
 });
+
+socket.on('chat-message-receive', (user,msg) => {
+    var timestamp=moment().format("DD-MM-YYYY HH:mm")
+    socket.broadcast.emit('chat-message-send',user,msg,timestamp);
+    socket.emit('chat-message-send',"Me",msg,timestamp);
+    console.log('message: ' + msg);
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message',(msg) => {
-        io.emit('chat message',msg);
-        console.log('message: ' + msg);
-    });
 });
 
 server.listen(3000,() => {
